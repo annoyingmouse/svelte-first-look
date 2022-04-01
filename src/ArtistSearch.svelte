153c39e0323997c5d7ae5a707aae53772f72caa8
Dominic Myers <annoyingmouse@gmail.com>
2022-04-01 15:26:45 +0100

    First commit

---
+++ src/ArtistSearch.svelte
@@ -0,0 +1,31 @@
+<script>
+  import {createEventDispatcher} from "svelte"
+  const dispatch = createEventDispatcher();
+  export let searchTerm
+</script>
+
+<div class="row justify-content-center my-2">
+  <div class="col-12 col-lg-6 border rounded bg-light p-3">
+    <h4 class="mb-1">
+      Search:
+      <small>
+        {#if searchTerm}
+          for
+        {/if}
+        {searchTerm}
+      </small>
+    </h4>
+    <div class="input-group">
+      <input type="text"
+             aria-label="Search Input"
+             class="form-control"
+             bind:value={searchTerm}
+             on:keyup={() => {
+               dispatch('updateSearch')
+             }}>
+    </div>
+  </div>
+
+</div>
+
+
